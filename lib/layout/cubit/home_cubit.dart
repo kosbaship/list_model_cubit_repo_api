9b8857711815f005c856eff880ec16db67ba0bef
kosba <mohamed.kosba96@gmail.com>
2020-12-26 23:04:32 +0200

    Conditional Builder & Cascade Notation (main.dart) & Parse inside Cubit

--- lib/layout/cubit/home_cubit.dart
+++ lib/layout/cubit/home_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:list_model_cubit_repo_api/data/models/post_model.dart';
 import 'package:list_model_cubit_repo_api/data/repository/repository.dart';
 import 'package:list_model_cubit_repo_api/layout/cubit/home_states.dart';
 
@@ -11,13 +12,12 @@ class HomeCubit extends Cubit<HomeStates> {
   List cubitPosts = [];
 
   getPosts() {
-    print("=================  State now is loading");
     emit(HomeLoadingState());
 
     repository.fetchData(path: "posts").then((value) {
-      print("=================  State now is Success");
+      cubitPosts =
+          (value.data as List).map((json) => PostModel.fromJson(json)).toList();
       emit(HomeSuccessState());
-      cubitPosts = value.data;
     }).catchError((e) {
       print(e.toString());
       emit(HomeErrorState(e.toString()));
--- lib/layout/home_screen.dart
+++ lib/layout/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:list_model_cubit_repo_api/data/models/post.dart';
 import 'package:list_model_cubit_repo_api/layout/cubit/home_cubit.dart';
 import 'package:list_model_cubit_repo_api/layout/cubit/home_states.dart';
 import 'package:list_model_cubit_repo_api/shared/components.dart';
@@ -17,13 +16,10 @@ class HomeScreen extends StatelessWidget {
         return ConditionalBuilder(
           condition: state is! HomeLoadingState,
           builder: (context) => ListView.builder(
-            // modify the list length
             itemCount: renderList.length,
             itemBuilder: (BuildContext context, int index) {
-              // ================= هنا ======================
-              // this list to get post by index
-              Post(renderList[index]);
-              // this is the list item representation out design
+              var post = renderList[index];
+
               return Card(
                 elevation: 8,
                 margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
@@ -33,19 +29,19 @@ class HomeScreen extends StatelessWidget {
                     child: ListTile(
                       // one
                       leading: defaultText(
-                        title: "${Post.getID()}",
+                        title: "${post.id}",
                         fontFamily: "Pacifico",
                         fontWeight: FontWeight.normal,
                       ),
                       //two
                       title: defaultText(
-                        title: "${Post.getTitle()}",
+                        title: "${post.title}",
                         fontFamily: "Pacifico",
                         fontWeight: FontWeight.normal,
                       ),
                       //three
                       subtitle: defaultText(
-                        title: "${Post.getBody()}",
+                        title: "${post.body}",
                       ),
                     )),
               );
